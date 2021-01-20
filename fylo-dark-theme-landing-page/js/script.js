1651eefb9b9f2b4ed2b834bc93ae7124c00e1a8f
Loris Bettazza <loris.bettazza@gmail.com>
2021-01-20 13:57:30 +0100

    :sparkles: Finish challenge "fylo-dark-theme-landing-page"

---
+++ fylo-dark-theme-landing-page/js/script.js
@@ -0,0 +1,40 @@
+// Functions
+function submitFormHandler(e) {
+  e.preventDefault();
+  alert('Form submitted succesfully!');
+}
+
+function invalidInputHandler(e) {
+  const input = e.target;
+  const id = input.getAttribute('id');
+  const errorLabel = input
+    .closest('form')
+    .querySelector(`.signup-form__error[for=${id}]`);
+
+  e.preventDefault();
+  errorLabel.textContent = input.validationMessage;
+}
+
+function changeInputHandler(e) {
+  const input = e.target;
+
+  if (!input.validity.valid) return;
+
+  const id = input.getAttribute('id');
+  const errorLabel = input
+    .closest('form')
+    .querySelector(`.signup-form__error[for=${id}]`);
+
+  errorLabel.textContent = '';
+}
+
+// Variables
+const form = document.querySelector('.js-signup-form');
+const inputs = form.querySelectorAll('input');
+
+// Events
+form.addEventListener('submit', submitFormHandler);
+inputs.forEach(input => {
+  input.addEventListener('invalid', invalidInputHandler);
+  input.addEventListener('change', changeInputHandler);
+});
