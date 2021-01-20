f0ef2ea2d9ce7f3cb33ba83331a4f6a405399ae0
Loris Bettazza <loris.bettazza@gmail.com>
2021-01-20 13:59:42 +0100

    :speech_balloon: Fix double description text

--- js/script.js
+++ js/script.js
@@ -124,7 +124,7 @@ const projects = [
     slug: 'testimonials-grid-section',
     tags: ['html', 'css'],
     description:
-      "This challenge will be perfect practice for anyone wanting to test their CSS Grid skills. Grid is such a powerful addition to CSS, so it's worth getting to grips with it!This challenge will be perfect practice for anyone wanting to test their CSS Grid skills. Grid is such a powerful addition to CSS, so it's worth getting to grips with it!",
+      "This challenge will be perfect practice for anyone wanting to test their CSS Grid skills. Grid is such a powerful addition to CSS, so it's worth getting to grips with it!",
   },
   {
     name: 'Pricing component with toggle',
